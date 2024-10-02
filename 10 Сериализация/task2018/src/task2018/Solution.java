37608197f51c0722be84c8673d5d8d6e208a4167
Yury <yury.connect@gmail.com>
2024-10-02 23:55:56 +0300

    solution: task2018

--- 10 Сериализация/task2018/src/task2018/Solution.java
+++ 10 Сериализация/task2018/src/task2018/Solution.java
@@ -23,6 +23,7 @@ Requirements:
 5. Программа должна выполняться без ошибок.
 6. При десериализации должны корректно восстанавливаться значение полей nameA и nameB.*/
 
+
 public class Solution {
     public static class A {
 
@@ -31,25 +32,47 @@ public class Solution {
         public A(String nameA) {
             this.nameA += nameA;
         }
+
+        public A() { // я добавил  Конструктор по умолчанию для A
+        }
     }
 
-    public class B extends A implements Serializable {
+
+
+    public static class B extends A implements Serializable { // сделал класс статическим
 
         private String nameB;
 
         public B(String nameA, String nameB) {
             super(nameA);
-            this.nameA += nameA;
+//            this.nameA += nameA;
             this.nameB = nameB;
         }
+
+        public B() { // я добавил  Конструктор по умолчанию
+        }
+
+        // Метод для сериализации
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            out.defaultWriteObject(); // Сериализуем поля класса B
+            out.writeUTF(nameA); // Сериализуем поле класса A
+        }
+
+        // Метод для десериализации
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            in.defaultReadObject(); // Десериализуем поля класса B
+            this.nameA = in.readUTF(); // Десериализуем поле класса A
+        }
     }
+
+
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
         ObjectOutputStream oos = new ObjectOutputStream(arrayOutputStream);
 
         Solution solution = new Solution();
-        B b = solution.new B("B2", "C33");
+        B b = new B("B2", "C33");
         System.out.println("nameA: " + b.nameA + ", nameB: " + b.nameB);
 
         oos.writeObject(b);
