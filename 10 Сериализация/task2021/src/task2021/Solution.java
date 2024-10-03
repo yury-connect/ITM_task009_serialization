5508e88f2e3d8426077a310d62b732f8dce97e97
Yury_Home_PC <yury.connect@gmail.com>
2024-10-04 00:31:22 +0300

    solution: task2021

--- 10 Сериализация/task2021/src/task2021/Solution.java
+++ 10 Сериализация/task2021/src/task2021/Solution.java
@@ -15,11 +15,40 @@ Requirements:
 4. При попытке сериализовать объект типа SubSolution должно возникать исключение NotSerializableException.
 5. При попытке десериализовать объект типа SubSolution должно возникать исключение NotSerializableException.*/
 
+
 public class Solution implements Serializable {
-    public static class SubSolution extends Solution {
+
+    /*
+Для того чтобы запретить сериализацию класса SubSolution, можно переопределить методы writeObject и readObject в этом
+классе и выбросить исключение NotSerializableException внутри них. Это приведет к тому, что при любой попытке
+сериализовать или десериализовать объект типа SubSolution, программа выбросит соответствующее исключение.
+     */
+    public static class SubSolution extends Solution {// Переопределяем метод для сериализации
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            throw new NotSerializableException("Сериализация класса SubSolution запрещена.");
+        }
+
+        // Переопределяем метод для десериализации
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            throw new NotSerializableException("Десериализация класса SubSolution запрещена.");
+        }
     }
 
     public static void main(String[] args) {
+        SubSolution subSolution = new SubSolution();
 
+        // Пример проверки на сериализацию
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("subSolution.ser"))) {
+            oos.writeObject(subSolution);
+        } catch (IOException e) {
+            System.out.println("Исключение: " + e.getMessage());
+        }
+
+        // Пример проверки на десериализацию
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("subSolution.ser"))) {
+            SubSolution deserializedSubSolution = (SubSolution) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Исключение: " + e.getMessage());
+        }
     }
 }
